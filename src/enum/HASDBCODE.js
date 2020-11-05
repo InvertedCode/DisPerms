80f2789fd9064ff0ee13759e87f041b42c85e48c
X-San <ytbaconboi95@gmail.com>
2020-11-05 19:05:03 +0000

    enum docs

--- src/enum/HASDBCODE.js
+++ src/enum/HASDBCODE.js
@@ -1,6 +1,21 @@
+/**
+ * These are the possible return values from {@link PermissionManager.hasDatabase}.
+ * @enum {number}
+ * @readonly
+ * @since 3.0.0
+ */
 const HASDBCODE = {
+  /**
+   * The database is actively loaded into memory
+   */
   "DB_ACTIVE": 0,
+  /**
+   * The database exists, but is not loaded
+   */
   "DB_EXISTS": 1,
+  /**
+   * The database is less real than Discord API V7
+   */
   "DB_NODB":   2
 }
 
